4e9a5053f4cab60e375dcd7218526e5159e93345
Bryan Anthony Garcia <bryananthonygarcia@live.com>
2019-07-02 16:47:13 +0800

    Initial Commit

--- FoodPredictionApp/FoodPredictionApp/MainPage.xaml.cs
+++ FoodPredictionApp/FoodPredictionApp/MainPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +12,82 @@ namespace FoodPredictionApp
 {
     public partial class MainPage : ContentPage
     {
+        private Stream stream;
+
         public MainPage()
         {
             InitializeComponent();
+
+            CrossMedia.Current.Initialize();
+
+            pickPhoto.Clicked += async (sender, args) =>
+            {
+                if (!CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    await DisplayAlert("Photos Not Supported", ":( Permission not granted to photos.", "OK");
+                    return;
+                }
+                var file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+
+                });
+
+
+                if (file == null)
+                    return;
+
+                activityIndicator.IsVisible = true;
+                stream = file.GetStream();
+                activityIndicator.IsVisible = false;
+
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+            };
         }
+
+        private async void TakePhoto(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Test",
+                SaveToAlbum = true,
+                CompressionQuality = 75,
+                CustomPhotoSize = 50,
+                PhotoSize = PhotoSize.MaxWidthHeight,
+                MaxWidthHeight = 2000,
+                DefaultCamera = CameraDevice.Front
+            });
+
+            if (file == null)
+                return;
+
+            activityIndicator.IsVisible = true;
+            stream = file.GetStream();
+            activityIndicator.IsVisible = false;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+
+        public async Task IdentifyFood(Stream image)
+        {
+                
+        }
+
     }
 }
